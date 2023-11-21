f7545d8f92853a7f989318947361038bfd18bb21
timkingteam <timkingteam@gmail.com>
2023-11-21 15:36:10 +0100

    uploaded openai example

---
+++ aistringcmp.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="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"
+)
+
+first_message = "OPERATOR: NORMAL STOP ACTUATED"
+second_message = "sdafED"
+chat_completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You will be provided with 2 strings which represent machine faults. You should output one number: '1' if those strings represent the same or similar faults and '0' otherwise"},
+    {"role": "user", "content": f"First: '{first_message}' Second: '{second_message}'"}
+    ]
+)
+
+print(chat_completion.choices[0].message.content)
