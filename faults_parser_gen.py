fb2aaa691ca921a1b3fed5a35ee4e34669133a29
timkingteam <timkingteam@gmail.com>
2023-11-22 11:42:09 +0100

    faults parser presented format modification

--- faults_parser_gen.py
+++ faults_parser_gen.py
@@ -3,7 +3,8 @@ from masterdocx_reader import read_word_document_gen
 from openai_faults_cmp import faults_cmp
 
 # yields tuples: first element is the type (string): "missing" "presented" "not_matching"
-# missing will be in the format as in XLSX presented in format as in DOCX
+# missing will be in format (number, xlsx_name)
+# presented will be in format (number, xlsx_name, master_name)
 # not matching will contain 3 elements: number, xlsx_name, docx_name
 def faults_parser_gen(xls_path, master_word_path):
     for xlsx_number, xlsx_name in xls_read_gen(xls_path):
@@ -12,7 +13,7 @@ def faults_parser_gen(xls_path, master_word_path):
             if xlsx_number == master_number:
                 is_present = 1
                 if faults_cmp(xlsx_name, master_name):
-                    yield ("presented", master_number, master_name)
+                    yield ("presented", master_number, xlsx_name, master_name)
                 else:
                     yield ("not_matching", xlsx_number, xlsx_name, master_name)
         if not is_present:
